bb434960eb957e80a201f879c68d040f65715a67
Brendan Le Foll <brendan.le.foll@intel.com>
2015-12-02 15:03:19 +0000

    imraa: Do some very basic parsing
    Signed-off-by: Brendan Le Foll <brendan.le.foll@intel.com>

--- examples/imraa/imraa.c
+++ examples/imraa/imraa.c
@@ -54,49 +54,69 @@ print_command_error()
 }
 
 int
-parse_yaml()
-{
-    return 0;
-}
-
-int
 main(int argc, char** argv)
 {
-    if (argc > 1) {
+    if (argc > 2) {
         print_command_error();
     }
 
-#if 0
+    char* imraa_conf_file = IMRAA_CONF_FILE;
+
     if (argc > 1) {
         if (strcmp(argv[1], "help") == 0) {
             print_help();
         } else if (strcmp(argv[1], "version") == 0) {
             print_version();
+        } else {
+            imraa_conf_file = argv[1];
         }
     }
-#endif
 
-    FILE *fh = fopen(IMRAA_CONF_FILE, "r");
+    FILE *fh = fopen(imraa_conf_file, "r");
     if (fh == NULL) {
         fprintf(stderr, "Failed to open configuration file\n");
         return EXIT_FAILURE;
     }
+
+    yaml_token_t token;
     yaml_parser_t parser;
 
     /* Initialize parser */
     if (!yaml_parser_initialize(&parser)) {
         fprintf(stderr, "Failed to initialize parser!\n");
-    }
-
-    if (fh == NULL) {
-        fprintf(stderr, "Failed to open file!\n");
+        fclose(fh);
+        return EXIT_FAILURE;
     }
 
     /* Set input file */
     yaml_parser_set_input_file(&parser, fh);
 
-    parse_yaml();
+    do {
+        yaml_parser_scan(&parser, &token);
+        switch(token.type) {
+            /* Stream start/end */
+            case YAML_STREAM_START_TOKEN: puts("STREAM START"); break;
+            case YAML_STREAM_END_TOKEN:   puts("STREAM END");   break;
+            /* Token types (read before actual token) */
+            case YAML_KEY_TOKEN:   printf("(Key token)   "); break;
+            case YAML_VALUE_TOKEN: printf("(Value token) "); break;
+            /* Block delimeters */
+            case YAML_BLOCK_SEQUENCE_START_TOKEN: puts("<b>Start Block (Sequence)</b>"); break;
+            case YAML_BLOCK_ENTRY_TOKEN:          puts("<b>Start Block (Entry)</b>");    break;
+            case YAML_BLOCK_END_TOKEN:            puts("<b>End block</b>");              break;
+            /* Data */
+            case YAML_BLOCK_MAPPING_START_TOKEN:  puts("[Block mapping]");            break;
+            case YAML_SCALAR_TOKEN:  printf("scalar %s \n", token.data.scalar.value); break;
+            /* Others */
+            default:
+                printf("Got token of type %d\n", token.type);
+        }
+    if(token.type != YAML_STREAM_END_TOKEN)
+        yaml_token_delete(&token);
+    } while(token.type != YAML_STREAM_END_TOKEN);
 
+    // cleanup
+    yaml_token_delete(&token);
     yaml_parser_delete(&parser);
     fclose(fh);
 
